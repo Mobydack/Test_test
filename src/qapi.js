bfee034ac0b532b31495dceccd2d5ec8eed06bed
0x4B_0x49_0x52 <devzetsubou@gmail.com>
2021-01-28 16:59:50 +0300

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60000ec8a6aceb001f324b9c",
+				"60000ec8a6aceb001f324b9e"
+			]
+		},
+		"60000ec8a6aceb001f324b9c": {
+			"id": "60000ec8a6aceb001f324b9c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60000ec8a6aceb001f324b9e": {
+			"id": "60000ec8a6aceb001f324b9e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
